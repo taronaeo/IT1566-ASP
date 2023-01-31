d1240f8652abe45d3d6f24338aae00303a53ccac
Aaron Teo <aaron.teo@riv-alumni.com>
2023-02-01 02:39:48 +0800

    fix: bugfix next_url not used for redirect

--- website/routes/auth.py
+++ website/routes/auth.py
@@ -26,24 +26,28 @@ def login():
     return redirect(url_for('views.home'))
 
   if request.method == 'POST':
-    next_url = request.args.get('next')
-
     email = request.form.get('email')
     password = request.form.get('password')
+
+    next_url = request.args.get('next')
+    redirect_to = redirect(url_for('auth.login', next=next_url, email=email))
 
     # Check if email or password is empty
     if not email or not password:
       flash('Enter an email and password')
-      return redirect(url_for('auth.login'))
+      return redirect_to
 
     user = User.query_user(email)
     if not user:
       flash('Account does not exist')
-      return redirect(url_for('auth.login'))
+      return redirect_to
 
-    if user.password == password:
-      login_user(user, remember=True)
-      return redirect(next_url or url_for('views.home'))
+    if user.password != password:
+      flash('Incorrect email or password')
+      return redirect_to
+
+    login_user(user, remember=True)
+    return redirect(next_url or url_for('views.home'))
 
   return render_template('/auth/login.html',
                           user=current_user)
@@ -54,7 +58,6 @@ def signup():
     return redirect(url_for('views.home'))
 
   if request.method == 'POST':
-    next_url = request.args.get('next')
 
     full_name = request.form.get('full_name')
     phone_number = request.form.get('phone_number')
@@ -62,25 +65,32 @@ def signup():
     password = request.form.get('password')
     password_confirm = request.form.get('password_confirm')
 
+    next_url = request.args.get('next')
+    redirect_to = redirect(url_for('auth.signup',
+                                    next=next_url,
+                                    full_name=full_name,
+                                    phone_number=phone_number,
+                                    email=email))
+
     if not full_name or \
         not phone_number or \
         not email or \
         not password or \
         not password_confirm:
       flash('All fields must not be empty')
-      return redirect(url_for('auth.signup'))
+      return redirect_to
 
     if not re.match(r'^\d{4}\s\d{4}$', phone_number):
       flash('Invalid phone number')
-      return redirect(url_for('auth.signup'))
+      return redirect_to
 
     if not re.match(r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$", email):
       flash('Invalid email address')
-      return redirect(url_for('auth.signup'))
+      return redirect_to
 
     if password != password_confirm:
       flash('Password and Confirm Password must be the same')
-      return redirect(url_for('auth.signup'))
+      return redirect_to
 
     user = User.create(email, full_name, phone_number, password)
     Wallet.create_wallet(email)
