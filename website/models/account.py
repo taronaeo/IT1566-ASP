5611379eb81411b37174b88d73ce107463c28c09
Junjie <J_unj_ie@outlook.com>
2023-02-12 02:32:41 +0800

    Updated wallet ui + added popups

--- website/models/account.py
+++ website/models/account.py
@@ -9,7 +9,7 @@
 import shelve
 from .. import DB_USER_LOCATION, DB_WALLET_LOCATION, DB_LISTING_LOCATION, DB_REVIEW_LOCATION
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 from flask_login import current_user, login_required
 
 account = Blueprint('account', __name__)
@@ -17,7 +17,7 @@ account = Blueprint('account', __name__)
 @account.route('/account/wallet')
 def wallet():
   with shelve.open(DB_USER_LOCATION) as db_user, shelve.open(DB_WALLET_LOCATION) as db_wallet:
-      return render_template('/account/wallet.html', user=current_user,wallet=db_wallet[current_user.uid])
+      return render_template('/account/wallet.html',user=current_user,wallet=db_wallet[current_user.uid])
 
 
 @account.route('/account')
@@ -42,6 +42,21 @@ def profile():
       with shelve.open(DB_LISTING_LOCATION) as db_listing:
         with shelve.open(DB_REVIEW_LOCATION) as db_review:
           return render_template('/account/profile.html', user=current_user,wallet=db_wallet[current_user.uid],cars=db_listing, reviews=db_review)
+
+
+@account.route('/account/profile/<string:uid>')
+def other_profile(uid):
+  with shelve.open(DB_USER_LOCATION) as db_user:
+    with shelve.open(DB_WALLET_LOCATION) as db_wallet:
+      with shelve.open(DB_LISTING_LOCATION) as db_listing:
+        with shelve.open(DB_REVIEW_LOCATION) as db_review:
+          if uid == current_user.uid:
+            return redirect(url_for('account.profile',
+                                    user=current_user,
+                                    wallet=db_wallet[uid],
+                                    cars=db_listing,
+                                    reviews=db_review))
+          return render_template('/account/external_profile.html', user=db_user[uid],wallet=db_wallet[uid],cars=db_listing, reviews=db_review)
 
 
 @account.route('/account/inbox')
