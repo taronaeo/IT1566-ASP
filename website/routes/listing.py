150fddacccfbd2988c53005acd287e1dbfad19e1
Aaron Teo <aaron.teo@riv-alumni.com>
2023-02-13 06:08:04 +0800

    feat: linked job start and end together

--- website/routes/listing.py
+++ website/routes/listing.py
@@ -43,15 +43,15 @@ def jobstart(uid):
         flash('All exterior photos are required')
         return redirect(url_for('listing.jobstart', uid=uid))
 
-      front_of_vehicle_img.save(f'{UPLOAD_DIR}/{uid}_front_of_vehicle_img.jpg')
-      left_of_vehicle_img.save(f'{UPLOAD_DIR}/{uid}_left_of_vehicle_img.jpg')
-      right_of_vehicle_img.save(f'{UPLOAD_DIR}/{uid}_right_of_vehicle_img.jpg')
-      back_of_vehicle_img.save(f'{UPLOAD_DIR}/{uid}_back_of_vehicle_img.jpg')
+      front_of_vehicle_img.save(f'{UPLOAD_DIR}/{uid}_front_of_vehicle_bf.jpg')
+      left_of_vehicle_img.save(f'{UPLOAD_DIR}/{uid}_left_of_vehicle_bf.jpg')
+      right_of_vehicle_img.save(f'{UPLOAD_DIR}/{uid}_right_of_vehicle_bf.jpg')
+      back_of_vehicle_img.save(f'{UPLOAD_DIR}/{uid}_back_of_vehicle_bf.jpg')
 
-      listing.exterior_photos.append(f'{uid}_front_of_vehicle_img.jpg')
-      listing.exterior_photos.append(f'{uid}_left_of_vehicle_img.jpg')
-      listing.exterior_photos.append(f'{uid}_right_of_vehicle_img.jpg')
-      listing.exterior_photos.append(f'{uid}_back_of_vehicle_img.jpg')
+      listing.exterior_photos.append(f'{uid}_front_of_vehicle_bf.jpg')
+      listing.exterior_photos.append(f'{uid}_left_of_vehicle_bf.jpg')
+      listing.exterior_photos.append(f'{uid}_right_of_vehicle_bf.jpg')
+      listing.exterior_photos.append(f'{uid}_back_of_vehicle_bf.jpg')
 
       if interior1_img:
         interior1_img.save(f'{UPLOAD_DIR}/{uid}_interior1.jpg')
@@ -69,6 +69,7 @@ def jobstart(uid):
         interior4_img.save(f'{UPLOAD_DIR}/{uid}_interior4.jpg')
         listing.interior_photos.append(f'{uid}_interior4.jpg')
 
+      listing.status = 'STARTED'
       db_listing[uid] = listing
       flash('Successfully started job')
       return redirect(url_for('listing.jobend', uid=uid))
@@ -76,21 +77,58 @@ def jobstart(uid):
   return render_template('/job/jobstart.html', user=current_user)
 
 
-@listing.route('/job/jobend')
-def jobend():
+@listing.route('/cars/<string:uid>/end', methods=['GET', 'POST'])
+def jobend(uid):
   if request.method == 'POST':
-    vehicle_img = request.files.get('vehicle_img')
+    with shelve.open(DB_CAR_LISTING_LOCATION) as db_listing:
+      listing = db_listing[uid]
 
-    if not vehicle_img:
+      front_of_vehicle_img = request.files.get('front_of_vehicle_img')
+      left_of_vehicle_img = request.files.get('left_of_vehicle_img')
+      right_of_vehicle_img = request.files.get('right_of_vehicle_img')
+      back_of_vehicle_img = request.files.get('back_of_vehicle_img')
 
-      flash('All fields must not be empty')
-      return redirect(request.url)
+      interior1_img = request.files.get('interior1_img')
+      interior2_img = request.files.get('interior2_img')
+      interior3_img = request.files.get('interior3_img')
+      interior4_img = request.files.get('interior4_img')
 
-    if not check_filename(vehicle_img.filename):
-      flash('Invalid file type. Only PNG and JPG files are accepted')
-      return redirect(request.url)
+      if not front_of_vehicle_img or \
+              not left_of_vehicle_img or \
+              not right_of_vehicle_img or \
+              not back_of_vehicle_img:
+        flash('All exterior photos are required')
+        return redirect(url_for('listing.jobstart', uid=uid))
 
-    filename = secure_filename(vehicle_img.filename)  # type: ignore
-    vehicle_img.save(f'{UPLOAD_DIR}/{filename}')
+      front_of_vehicle_img.save(f'{UPLOAD_DIR}/{uid}_front_of_vehicle_af.jpg')
+      left_of_vehicle_img.save(f'{UPLOAD_DIR}/{uid}_left_of_vehicle_af.jpg')
+      right_of_vehicle_img.save(f'{UPLOAD_DIR}/{uid}_right_of_vehicle_af.jpg')
+      back_of_vehicle_img.save(f'{UPLOAD_DIR}/{uid}_back_of_vehicle_af.jpg')
+
+      listing.exterior_photos.append(f'{uid}_front_of_vehicle_af.jpg')
+      listing.exterior_photos.append(f'{uid}_left_of_vehicle_af.jpg')
+      listing.exterior_photos.append(f'{uid}_right_of_vehicle_af.jpg')
+      listing.exterior_photos.append(f'{uid}_back_of_vehicle_af.jpg')
+
+      if interior1_img:
+        interior1_img.save(f'{UPLOAD_DIR}/{uid}_interior1.jpg')
+        listing.interior_photos.append(f'{uid}_interior1.jpg')
+
+      if interior2_img:
+        interior2_img.save(f'{UPLOAD_DIR}/{uid}_interior2.jpg')
+        listing.interior_photos.append(f'{uid}_interior2.jpg')
+
+      if interior3_img:
+        interior3_img.save(f'{UPLOAD_DIR}/{uid}_interior3.jpg')
+        listing.interior_photos.append(f'{uid}_interior3.jpg')
+
+      if interior4_img:
+        interior4_img.save(f'{UPLOAD_DIR}/{uid}_interior4.jpg')
+        listing.interior_photos.append(f'{uid}_interior4.jpg')
+
+      listing.status = 'COMPLETED'
+      db_listing[uid] = listing
+      flash('Successfully ended job')
+      return redirect(url_for('car_listing.cars', uid=uid))
 
   return render_template('/job/jobend.html', user=current_user)
